4254c59ef205b91f842ad7085c8353cfa9595ea9
Sachin Kandel <sachinkandel78@gmail.com>
2026-08-08 13:17:28 +0545

    Memory added(Comleted Langchain implementation successfully

--- examples1.py
+++ examples1.py
@@ -6,6 +6,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_classic.chains import LLMChain
 
 from langchain_classic.chains import SequentialChain
+from langchain_classic.memory import ConversationBufferMemory
 
 os.environ["GROQ_API_KEY"] = groq_api_key
 
@@ -20,9 +21,14 @@ first_input_prompt = PromptTemplate(
     template="Tell me about celebrity {name}.",
 )
 
+## memory for the LLM
+person_memory = ConversationBufferMemory(input_key="name", memory_key="person", output_key="person")
+dob_memory = ConversationBufferMemory(input_key="person", memory_key="dob", output_key="dob")
+description_memory = ConversationBufferMemory(input_key="dob", memory_key="description", output_key="description")
+
 ## groq LLM
 llm = ChatGroq( model="llama-3.1-8b-instant",  temperature=0.8 )
-chain = LLMChain(llm=llm, prompt=first_input_prompt, verbose=True, output_key="person")
+chain = LLMChain(llm=llm, prompt=first_input_prompt, verbose=True, output_key="person", memory=person_memory)
 
 ## Prompt template for the LLM
 
@@ -30,15 +36,22 @@ second_input_prompt = PromptTemplate(
     input_variables=["person"],
     template="when was {person} born?",
 )
-chain2 = LLMChain(llm=llm, prompt=second_input_prompt, verbose=True, output_key="dob")
+chain2 = LLMChain(llm=llm, prompt=second_input_prompt, verbose=True, output_key="dob", memory=dob_memory)
 third_input_prompt = PromptTemplate(
     input_variables=["dob"],
     template="Mention 5 major events happened around {dob} in the world.",
 )
-chain3 = LLMChain(llm=llm, prompt=third_input_prompt, verbose=True, output_key="description")
+chain3 = LLMChain(llm=llm, prompt=third_input_prompt, verbose=True, output_key="description", memory=description_memory)
 
 parent_chain = SequentialChain(
     chains=[chain, chain2, chain3], input_variables=['name'], output_variables=['person','dob','description'], verbose=True)
 
 if input_text:
     st.write(parent_chain.invoke({"name": input_text}))
+
+    with st.expander('Person name'):
+        st.write(person_memory.buffer)
+    
+
+    with st.expander('Description'):
+        st.write(description_memory.buffer)
