5c5054c88c97d021865427f2322f7c908d6994b1
Sachin Kandel <sachinkandel78@gmail.com>
2026-08-08 12:59:57 +0545

    Replace SimpleSequentialChain with Sequential chain

--- examples1.py
+++ examples1.py
@@ -5,6 +5,7 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_classic.chains import LLMChain
 
+from langchain_classic.chains import SequentialChain
 
 os.environ["GROQ_API_KEY"] = groq_api_key
 
@@ -20,12 +21,18 @@ first_input_prompt = PromptTemplate(
 )
 
 ## groq LLM
-llm = ChatGroq(
-    model="llama-3.1-8b-instant",
-    temperature=0.8
-)
-chain = LLMChain(llm=llm, prompt=first_input_prompt, verbose=True)
+llm = ChatGroq( model="llama-3.1-8b-instant",  temperature=0.8 )
+chain = LLMChain(llm=llm, prompt=first_input_prompt, verbose=True, output_key="person")
 
+## Prompt template for the LLM
+
+second_input_prompt = PromptTemplate(
+    input_variables=["person"],
+    template="when was {person} born?",
+)
+chain2 = LLMChain(llm=llm, prompt=second_input_prompt, verbose=True, output_key="dob")
+parent_chain = SequentialChain(
+    chains=[chain, chain2], input_variables=['name'], output_variables=['person','dob'], verbose=True)
 
 if input_text:
-    st.write(chain.run(input_text))
+    st.write(parent_chain.invoke({"name": input_text}))
